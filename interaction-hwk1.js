1d83c1059a68a47309cd785bb5cb0ee64ee5412e
malabasterr <113557081+malabasterr@users.noreply.github.com>
2023-07-30 01:33:44 +0100

    structure for files

---
+++ interaction-hwk1.js
@@ -0,0 +1,8 @@
+let amountOne = document.querySelector("#amountOne");
+let amountTwo = document.querySelector("#amountTwo");
+
+const addValues = function addValues(a, b) {
+  return a + b;
+};
+
+let result = addValues(amountOne, amountTwo);
